279ed112ae1de7ee44a8661330d467e440bafefd
jhchoi1182 <jhchoi1182@gmail.com>
2022-12-20 20:47:14 +0900

    컴포넌트 수정

--- src/components/pages/Todos.jsx
+++ src/components/pages/Todos.jsx
@@ -3,12 +3,16 @@ import Chick from "../shared/Chick";
 import Header from "../shared/Header";
 import login from "../../images/login.webp";
 import lv4 from "../../images/LV4.webp";
+import StForm from "../ui/div/StForm";
+import StInput from "../ui/inputs/StInput";
 
 const Todos = () => {
   return (
     <div>
       <Header />
-      <Chick src={lv4} />
+      <Chick src={login} />
+      <StForm>
+      </StForm>
     </div>
   );
 };
